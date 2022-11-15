53a535ec689e72c431f39dfc593ec3971b8cd8b1
Nimna Thiranjaya <nimnathiranjaya523@gmail.com>
2022-11-15 15:17:11 +0530

    Create docker file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.status(200).send({status : true, message : "Hello world"})
+});
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
